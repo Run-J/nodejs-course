dc803e51b50995ab3e7838367e9b8fba6fc79727
Run-J <jirun040403@hotmail.com>
2025-10-21 20:40:15 +0800

    Creating a Search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,22 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=Boston').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            return console.log(data.error)
-        }
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
 
-        console.log(data.location)
-        console.log(data.forecast)
-    })
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    if (location) {
+        fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    return console.log(data.error)
+                }
+
+                console.log(data.location)
+                console.log(data.forecast)
+            })
+        })
+    }
 })
