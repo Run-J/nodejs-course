9c46f42d94c9d6c8858e68e3425a69918bb9bdf4
Run-J <jirun040403@hotmail.com>
2025-10-22 20:17:27 +0800

    Wiring up the User Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,21 +2,31 @@ console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
     if (location) {
         fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
             response.json().then((data) => {
                 if (data.error) {
+                    messageOne.textContent = data.error
                     return console.log(data.error)
                 }
 
-                console.log(data.location)
-                console.log(data.forecast)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             })
         })
+    } else {
+        messageOne.textContent = 'Location cannot be empty.'
     }
 })
